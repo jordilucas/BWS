9462468cb62d14e036946a2782103eca61905062
Jordi Lucas <jordilucas3@gmail.com>
2015-10-27 20:44:50 -0300

    Lib Volley

--- app/src/main/java/br/com/bwsmobile/bwsmobile/AlmoxarifadoActivity.java
+++ app/src/main/java/br/com/bwsmobile/bwsmobile/AlmoxarifadoActivity.java
@@ -6,13 +6,24 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class AlmoxarifadoActivity extends AppCompatActivity {
+
+    Button btnAlmoxarifado, btnManutencao, btnAbastecimento, btnUsoFrota, btnCartoes, btnProtocolo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_almoxarifado);
+
+        btnAlmoxarifado = (Button)findViewById(R.id.btnAlmoxarifado);
+        btnManutencao = (Button)findViewById(R.id.btnManutencao);
+        btnAbastecimento = (Button)findViewById(R.id.btnAbastecimentos);
+        btnUsoFrota = (Button)findViewById(R.id.btnUsoDeFrota);
+        btnCartoes = (Button)findViewById(R.id.btnCartoes);
+        btnProtocolo = (Button)findViewById(R.id.btnProtocolo);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -24,7 +35,7 @@ public class AlmoxarifadoActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
